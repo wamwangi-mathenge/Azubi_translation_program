6ecdd90b1c73c4ac877fa29df503e1f68ef84fbc
Brian <wamwangimathenge@gmail.com>
2023-08-31 16:16:36 +0300

    Ask for a color and give its German translation

--- translate.py
+++ translate.py
@@ -15,3 +15,8 @@ german2 = input("Input its German translation: ")
 colors[english1] = german1
 colors[english2] = german2
 print(colors)
+
+## Exercise 2
+eng_color = input("Which color should be translated? ")
+
+print(f"The German word for {eng_color} is {colors[eng_color]}")
